518a3a0aa2407965cb636dbf48310a27b014932b
FaBiUsKcomp <fabio.oliveira2@computacao.ufla.br>
2019-11-22 09:01:46 -0300

    Comentarios atualizados

--- buskTeste/static/js/alertas.js
+++ buskTeste/static/js/alertas.js
@@ -1,15 +1,19 @@
+//Função para Enviar o formulário do registro do livro
 function sendRegisterBook() {
   document.formResgisterBook.submit();
 }
 
+//Função para Enviar o formulário da alteração do livro
 function sendUpdateBook() {
   document.formUpdateBook.submit();
 }
 
+//Função para Enviar o ISBN de deleção do livro
 function sendDeleteBook() {
   document.formDeleteBook.submit();
 }
 
+//Tela de Sucesso do cadastro
 function showSuccessRegister() {
   Swal.fire({
     'title': 'Livro Cadastrado com Sucesso!',
@@ -19,6 +23,7 @@ function showSuccessRegister() {
   })
 }
 
+//Tela de Sucesso da deleção
 function showSuccessDelete() {
   Swal.fire({
     'title': 'Livro Excluído com Sucesso!',
@@ -28,6 +33,7 @@ function showSuccessDelete() {
   })
 }
 
+//Tela de Sucesso da alteração
 function showSuccessUpdate() {
   Swal.fire({
     'title': 'Livro Atualizado com Sucesso!',
